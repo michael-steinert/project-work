8692cc632ac1ce509da712042e6c57515e515d88
Michael Steinert <michael.steinert.94@gmail.com>
2020-11-21 11:57:21 +0100

    fixed untracked files

--- Backend/microservices/manufacturing-department/src/main/resources/db/migration/V1__CreateBikeTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS bike (
-    bike_id UUID PRIMARY KEY NOT NULL,
-    bike_name VARCHAR(250) NOT NULL,
-    description TEXT NOT NULL,
-    short_description TEXT NOT NULL,
-    bike_type VARCHAR(12) NOT NULL,
-    price FLOAT NOT NULL
-);
