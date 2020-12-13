fe08be42654a64df22b08e62e547bac077c993d5
Michael Steinert <michael.steinert.94@gmail.com>
2020-12-13 20:52:07 +0100

    Added Create and Delete Function to Frontend

--- frontend/sales-department/src/OrderList.jsx
+++ frontend/sales-department/src/OrderList.jsx
@@ -7,7 +7,12 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import axios from "axios";
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
+import SearchIcon from '@material-ui/icons/Search';
+import {createNewOrder, deleteOrderById, getAllOrders} from "./Client";
+import {Button, TableFooter} from "@material-ui/core";
 
 const useStyles = makeStyles({
     table: {
@@ -15,23 +20,42 @@ const useStyles = makeStyles({
     },
 });
 
-const api = axios.create({
-    baseURL: 'http://localhost:8080/api/v1/order',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-});
-
 class CustomerList extends Component {
     state = {orders: []}
 
-    constructor() {
-        super();
-        this.getOrders();
+    constructor(props) {
+        super(props);
+        this.state = {
+            orders: [],
+            createOderFeedback: 0
+        };
+        this.readAllOrdersFromApi();
     }
 
-    getOrders = async () => {
-        await api.get('/orders').then(res => this.setState({orders: res.data})).catch(e => console.log(e))
+    readAllOrdersFromApi() {
+        getAllOrders().then(data => this.setState({orders:data}));
+    }
+
+    readOrderByIdFromApi(orderUid, order) {
+
+    }
+
+    createOrderFromApi() {
+        const data = {
+            customerUid: "c5d69be4-13cb-493e-902a-9b98c87a05cc",
+            bikeUid: "74f2c2de-2841-11eb-adc1-0242ac120082",
+            totalPrice: 899.94,
+            orderDate: "2020-12-13"
+        }
+        createNewOrder(data).then(data => this.setState({createOrderFeedback:data}));
+    }
+
+    updateOrderByIdFromApi(orderUid) {
+
+    }
+
+    deleteOrderByIdFromApi(orderUid) {
+        deleteOrderById(orderUid.orderUid)
     }
 
     render() {
@@ -53,9 +77,34 @@ class CustomerList extends Component {
                                 <TableCell>{order.bikeUid}</TableCell>
                                 <TableCell>{order.totalPrice}</TableCell>
                                 <TableCell>{order.orderDate}</TableCell>
+                                <TableCell>
+                                    <Button aria-label="Edit Order" onClick="">
+                                        <EditIcon/>
+                                    </Button>
+                                </TableCell>
+                                <TableCell>
+                                    <Button aria-label="Delete Order" onClick={() => this.deleteOrderByIdFromApi({orderUid : order.orderUid})}>
+                                        <DeleteIcon/>
+                                    </Button>
+                                </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
+                    <TableFooter>
+                        <TableRow>
+                            <TableCell>
+                                <Button aria-label="Delete Order" onClick={() => this.createOrderFromApi()}>
+                                    <AddIcon/>
+                                </Button>
+                                <Button aria-label="Delete Order" onClick={() => this.readOrderByIdFromApi()}>
+                                    <SearchIcon/>
+                                </Button>
+                                <div>
+                                    Der State: {this.state.createOrderFeedback}
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                    </TableFooter>
                 </Table>
             </TableContainer>
         )
