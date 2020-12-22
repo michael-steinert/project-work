f0f6b47c1edb35a56565384eed2ce00812f7685e
Michael Steinert <michael.steinert.94@gmail.com>
2020-12-22 21:55:09 +0100

    Initial Commit

---
+++ backend/circuit-breaker-dashboard/src/main/java/com/bike_factory/circuitbreakerdashboard/CircuitBreakerDashboardApplication.java
@@ -0,0 +1,18 @@
+package com.bike_factory.circuitbreakerdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrixDashboard
+public class CircuitBreakerDashboardApplication {
+
+	//Gateway-Service Stream for Hystrix-Dashboard: http://localhost:8091/actuator/hystrix.stream
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitBreakerDashboardApplication.class, args);
+	}
+
+}
