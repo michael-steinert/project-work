74450cd5399be361fe99871db64c19073b258497
Michael Steinert <michael.steinert.94@gmail.com>
2020-12-22 21:58:23 +0100

    Added Gateway-Service from Spring Cloud

--- backend/gateway-service/src/main/java/com/bike_factory/gatewayservice/GatewayServiceApplication.java
+++ backend/gateway-service/src/main/java/com/bike_factory/gatewayservice/GatewayServiceApplication.java
@@ -3,9 +3,11 @@ package com.bike_factory.gatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
