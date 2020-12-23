a828a87f0e9bdb2010d8db6ac9a7ff6c0db6599c
Michael Steinert <michael.steinert.94@gmail.com>
2020-12-23 18:53:47 +0100

    Added Circuit Breaker Dashboard

--- backend/circuit-breaker-dashboard/src/main/java/com/bike_factory/circuitbreakerdashboard/CircuitBreakerDashboardApplication.java
+++ backend/circuit-breaker-dashboard/src/main/java/com/bike_factory/circuitbreakerdashboard/CircuitBreakerDashboardApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableTurbine
 @EnableEurekaClient
 @EnableHystrixDashboard
 public class CircuitBreakerDashboardApplication {
 
-	//Gateway-Service Stream for Hystrix-Dashboard: http://localhost:8091/actuator/hystrix.stream
+	//Gateway-Service Stream for Hystrix-Dashboard: http://localhost:8092/actuator/hystrix.stream
 	public static void main(String[] args) {
 		SpringApplication.run(CircuitBreakerDashboardApplication.class, args);
 	}
