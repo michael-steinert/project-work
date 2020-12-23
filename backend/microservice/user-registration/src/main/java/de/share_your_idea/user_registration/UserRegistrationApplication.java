e265e96b856d85fdf3e7ca6a772f4bfb157dc1f9
Michael Steinert <michael.steinert.94@gmail.com>
2020-12-23 18:54:55 +0100

    Updated User Registration

--- backend/microservice/user-registration/src/main/java/de/share_your_idea/user_registration/UserRegistrationApplication.java
+++ backend/microservice/user-registration/src/main/java/de/share_your_idea/user_registration/UserRegistrationApplication.java
@@ -2,8 +2,10 @@ package de.share_your_idea.user_registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserRegistrationApplication {
 
 	public static void main(String[] args) {
