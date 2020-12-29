d5d50b5f8f93605461309383dd4bc6dee4b94bb1
Michael Steinert <michael.steinert.94@gmail.com>
2020-12-29 19:25:44 +0100

    Updated User Management Service

--- backend/microservice/user-management/src/main/java/de/share_your_idea/user_management/controller/config/ControllerConfig.java
+++ backend/microservice/user-management/src/main/java/de/share_your_idea/user_management/controller/config/ControllerConfig.java
@@ -4,6 +4,7 @@ import brave.sampler.Sampler;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
