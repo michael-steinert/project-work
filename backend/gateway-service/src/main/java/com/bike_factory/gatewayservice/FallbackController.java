b203e5e5645cef2a28ce4203770fe7fc7a513033
Michael Steinert <michael.steinert.94@gmail.com>
2020-12-29 16:46:52 +0100

    Added gitignore

---
+++ backend/gateway-service/src/main/java/com/bike_factory/gatewayservice/FallbackController.java
@@ -0,0 +1,20 @@
+package com.bike_factory.gatewayservice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+
+    @RequestMapping("/userRegistrationServiceFallBack")
+    public Mono<String> userRegistrationServiceFallBackMethod() {
+        return Mono.just("User Registration Service is taking longer than expected to respond or is down.");
+    }
+
+    @RequestMapping("/userManagementServiceFallBack")
+    public Mono<String> userManagementServiceFallBackMethod() {
+        return Mono.just("User Management Service is taking longer than expected to respond or is down.");
+    }
+}
+
