ab368aa3c2c125595aa315d6c853c6baaacdf740
Michael Steinert <michael.steinert.94@gmail.com>
2021-01-13 17:34:46 +0100

    Cahnged Port to localhost:8080

--- backend/microservice/user-registration/src/main/java/de/share_your_idea/user_registration/entity/AuthenticationRequest.java
+++
@@ -1,9 +0,0 @@
-package de.share_your_idea.user_registration.entity;
-
-import lombok.Data;
-
-@Data
-public class AuthenticationRequest {
-    private String username;
-    private String password;
-}
