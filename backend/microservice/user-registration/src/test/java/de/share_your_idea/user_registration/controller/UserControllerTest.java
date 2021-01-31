8f92194c0352e85fe8c1ff10bf03a3d83afd26ba
Michael Steinert <michael.steinert.94@gmail.com>
2021-01-31 20:05:20 +0100

    Added Test for UserController

---
+++ backend/microservice/user-registration/src/test/java/de/share_your_idea/user_registration/controller/UserControllerTest.java
@@ -0,0 +1,37 @@
+package de.share_your_idea.user_registration.controller;
+
+import de.share_your_idea.user_registration.entity.UserEntity;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest (webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class UserControllerTest {
+
+    @Autowired
+    private UserController userController;
+
+    @Test
+    public void shouldSaveUserEntity() throws Exception {
+        //Given
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUsername("Michael");
+        userEntity.setPassword("Password");
+        userEntity.setUser_role("ROLE_USER");
+
+        //When
+        userController.registerUser(userEntity);
+
+        //Then
+        UserEntity newUserEntity = userController.fetchUserByUsername(userEntity.getUsername()).getBody();
+        assertThat(newUserEntity.getUsername()).isEqualTo("Michael");
+        assertThat(newUserEntity.getUser_role()).isEqualTo("ROLE_USER");
+    }
+
+
+}
