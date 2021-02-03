79a76789073366cf58697187d49b724f44d9a05f
Michael Steinert <michael.steinert.94@gmail.com>
2021-02-03 21:21:51 +0100

    Added Configuration and Controller

--- backend/microservice/user-meeting-search/src/main/java/de/share_your_idea/usermeetingsearch/UserMeetingSearchApplication.java
+++ backend/microservice/user-meeting-search/src/main/java/de/share_your_idea/usermeetingsearch/UserMeetingSearchApplication.java
@@ -2,12 +2,12 @@ package de.share_your_idea.usermeetingsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserMeetingSearchApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(UserMeetingSearchApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(UserMeetingSearchApplication.class, args);
+    }
 }
---
+++ backend/microservice/user-meeting-search/src/main/java/de/share_your_idea/usermeetingsearch/controller/MainController.java
@@ -0,0 +1,25 @@
+package de.share_your_idea.usermeetingsearch.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class MainController {
+
+    @Value("${spring.application.name}")
+    private String serviceName;
+
+    @GetMapping(value = {"/", "/index"})
+    public String index(Model model) throws JsonProcessingException {
+        log.info("Main Controller: Index Method is called");
+        model.addAttribute("serviceName", serviceName);
+        log.info("Main Controller: Index Method created ServiceName : {}", new ObjectMapper().writeValueAsString(serviceName));
+        return "index";
+    }
+}
