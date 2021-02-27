c33222e90aec2d8a2f27aa9b557d56e460bda980
Michael Steinert <michael.steinert.94@gmail.com>
2021-02-27 20:42:31 +0100

    Replaced RestTemplate with OpenFeign

--- backend/microservices/user-meeting/src/main/java/de/share_your_idea/user_meeting/http_client/UserManagementServiceHTTPClient.java
+++ backend/microservices/user-meeting/src/main/java/de/share_your_idea/user_meeting/http_client/UserManagementServiceHTTPClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient("http://USER-MANAGEMENT-SERVICE/user-management/")
-public interface HTTPClient {
+public interface UserManagementServiceHTTPClient {
 
     @GetMapping("fetch-user-by-username/{username}")
     ResponseEntity<UserEntity> fetchUserByUsername(@PathVariable("username") String username);
